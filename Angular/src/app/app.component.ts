7d30c9b764bee9f69b39096e6b21f56ec3a8ce32
Lurisan <talk2lurisan@gmail.com>
2019-09-18 10:30:13 +0530

    Basic Content Sync Code added

--- Angular/src/app/app.component.ts
+++ Angular/src/app/app.component.ts
@@ -1,9 +1,101 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+  url = "http://192.168.0.109:8080"
+  downloadLoc = ''
+
+  constructor() { }
+
+  ngOnInit() {
+    document.addEventListener('deviceReady', function () {
+      console.log("device ready")
+      alert("device ready")
+    }, false)
+  }
+
+  sync() {
+    // @ts-ignore
+    var sync = ContentSync.sync({
+      url: this.url,
+      id: 'myapp',
+      type: 'local'
+    });
+    sync.on('complete', function (data) {
+      if (data.localPath) {
+        console.log("synced: ", data.localPath)
+        // var url = "file://" + data.localPath + "/www/index.html";
+        // alert('Sync complete ' + data + ' changing document.location ' + url);
+        // @ts-ignore
+        /* ContentSync.loadUrl(
+          url
+        ); */
+      }
+    });
+    sync.on('error', function (e) {
+      console.log("error in sync: ", e)
+    });
+  }
+
+  download(fileName) {
+    let that = this
+    let url = this.url + '/' + fileName
+    var callback = function (response) {
+      console.log(response);
+      if (response.progress) {
+        console.log(response.progress)
+      }
+      if (response.archiveURL) {
+        console.log(response.archiveURL);
+        that.downloadLoc = response.archiveURL;
+        alert("Downloaded");
+        // that.extract()
+      }
+    };
+    //@ts-ignore 
+    ContentSync.download(
+      url,
+      callback
+    );
+  }
+  extract() {
+
+    /* window.requestFileSystem(PERSISTENT, 1024 * 1024, function (fs) {
+      fs.root.getDirectory('zipOutPut', { create: true }, function (fileEntry) {
+        var dirUrl = fileEntry.toURL();
+        var callback = function (response) {
+          console.log(response);
+        }
+        console.log(dirUrl, archiveURL);
+        Zip.unzip(archiveURL, dirUrl, callback);
+        console.log(dirUrl)
+      });
+     });*/
+
+    //@ts-ignore
+    // downloader.init({ folder: "file:///storage/self/primary/AAOTA", unzip: true });
+    //@ts-ignore
+    // downloader.get(this.downloadLoc);
+    
+    window.resolveLocalFileSystemURL(this.downloadLoc, function (fileSystem) {
+      var reader = fileSystem.createReader();
+      reader.readEntries(
+        function (entries) {
+          console.log(entries);
+          console.log("extracted")
+          alert("extracted")
+        },
+        function (err) {
+          console.log(err);
+        }
+      );
+    }, function (err) {
+      console.log(err);
+    }
+    );
+  }
 }
--- Cordova/platforms/android/app/src/main/assets/www/cordova_plugins.js
+++ Cordova/platforms/android/app/src/main/assets/www/cordova_plugins.js
@@ -1,6 +1,18 @@
 cordova.define('cordova/plugin_list', function(require, exports, module) {
-  module.exports = [];
+  module.exports = [
+    {
+      "id": "phonegap-plugin-contentsync.ContentSync",
+      "file": "plugins/phonegap-plugin-contentsync/www/index.js",
+      "pluginId": "phonegap-plugin-contentsync",
+      "clobbers": [
+        "window.ContentSync",
+        "window.Zip",
+        "window.zip"
+      ]
+    }
+  ];
   module.exports.metadata = {
-    "cordova-plugin-whitelist": "1.3.4"
+    "cordova-plugin-whitelist": "1.3.4",
+    "phonegap-plugin-contentsync": "1.4.2"
   };
 });
